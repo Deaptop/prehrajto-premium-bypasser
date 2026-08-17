b100644f3fe460be1077b33e9672c194fc4012d2
Deaptop <davpa09@gmail.com>
2026-08-17 21:44:43 +0200

    Now cancels all tracking requests, which had been allowing the server to remember the page visit, Added comments

--- prehrajto_bypasser.js
+++ prehrajto_bypasser.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Prehraj.to Premium Bypasser
-// @description  Interferes play time check by overwriting redirect link parameter.
+// @description  Patches playtime limit and activity trackers
 // @match        https://prehrajto.cz/*
-// @version      0.1.0
+// @version      0.2.0
 // @run-at       document-start
 // @author       Deaptop
 // @grant        none
@@ -11,11 +11,41 @@
 (function () {
     'use strict';
 
-    Object.defineProperty(window, 'redirectLink', {
-        configurable: true,
-        get() {
+
+    // Patch playtime check
+
+    Object.defineProperty(window, 'redirectLink', { // defining a new, unchangeable property
+        configurable: true, // property can be later modified
+        get() { // reassigning get function to always return null
             return null;
         },
-        set(value) {}
+        set(value) {} // overwrite set to do nothing
     });
+    console.log('Playtime limit patched. A good start.')
+
+
+    // Intercept tracking XHR requests
+
+    // save the original functions
+    const originalOpen = XMLHttpRequest.prototype.open;
+    const originalSend = XMLHttpRequest.prototype.send;
+
+    originalOpen.call;
+
+    XMLHttpRequest.prototype.open = function(method, url, ...args) {// intercept
+        this.__blockVideoStarted = // bool value, was the specific XHR request started?
+            //method.toUpperCase() === 'POST' && //does it use POST method
+            ( String(url).includes('do=videoStarted') || String(url).includes('do=videoVisit') ); //does it contain blacklisted keys?
+
+        return originalOpen.call(this, method, url, ...args); //call original function
+    };
+
+    XMLHttpRequest.prototype.send = function(body) {
+        if (this.__blockVideoStarted) { // filter the wanted request
+            console.log('Blocked a tracker request. Good for you.');
+            return; // return without sending the request
+        }
+
+        return originalSend.call(this, body);// if passed, send the request
+    };
 })();
